9f3ea15d5437063778ca1cfb008815383d33a469
rajvrk2 <93917057+rajvrk2@users.noreply.github.com>
2026-02-18 09:53:21 +0530

    Update git2.sh

--- git2.sh
+++ git2.sh
@@ -5,5 +5,6 @@ NAME="Rajesh Kumar"
 echo "============================"
 echo "Hello, my name is $NAME"
 echo "Welcome to Shell Scripting!"
+echo "welcome to my world"
 echo "============================"
 
